5c7fd2154a4252083ff2a8ed309a8f69c5378b6e
Victor Bruce <victorbruce82@gmail.com>
2025-11-24 20:05:55 +0000

    test: update app.spec.ts test file

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -6,6 +6,7 @@ describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [{ provide: Store, useValue: {} }],
     }).compileComponents();
   });
 
